db2ffafbd4b55ff0dae1c8fc9141b3be2bd7f1ac
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2025-06-04 15:00:06 -0300

    feat: setup eslint

--- FrameWorks/NextJs/ignite-call/src/pages/_app.tsx
+++ FrameWorks/NextJs/ignite-call/src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import type { AppProps } from "next/app";
-import { globalStyles } from "../styles/global";
+import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
 
-globalStyles();
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
-  return <Component {...pageProps} />;
+  globalStyles()
+  return <Component {...pageProps} />
 }
