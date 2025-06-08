bdbdd4ce052c344068b083cae77b1dbd81d3adb3
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2025-06-08 16:36:30 -0300

    feat: using react hook form

--- FrameWorks/NextJs/ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ FrameWorks/NextJs/ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -2,15 +2,31 @@ import React from 'react'
 import { Form } from './styles'
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string().min(3, { message: 'Mínimo de 3 caracteres' }),
+})
+
+// Convertendo o schema para um tipo
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  const handleClaimUsername = (data: ClaimUsernameFormData) => {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size="sm"
         prefix="ignite.com/"
         placeholder="seu-usuario"
         required
+        {...register('username')}
       />
       <Button size="sm" type="submit">
         Reservar
