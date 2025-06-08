173ac15fedea43ece48aefc54b97c15883fd7c76
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2025-06-08 16:14:47 -0300

    style: home

---
+++ FrameWorks/NextJs/ignite-call/src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Container, Hero, Preview } from './styles'
+import { Heading, Text } from '@ignite-ui/react'
+import previewImage from '../../assets/preview.png'
+import Image from 'next/image'
+
+export default function HomePage() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl" as="h1">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário"
+          height={400}
+          quality={100}
+          priority={true}
+        />
+      </Preview>
+    </Container>
+  )
+}
