f8f4c15a0556a5c4d9094db1b12ae854b85e57f4
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2025-06-08 16:46:33 -0300

    feat: zod validation

--- FrameWorks/NextJs/ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ FrameWorks/NextJs/ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,37 +1,58 @@
 import React from 'react'
-import { Form } from './styles'
-import { Button, TextInput } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const claimUsernameFormSchema = z.object({
-  username: z.string().min(3, { message: 'Mínimo de 3 caracteres' }),
+  username: z
+    .string()
+    .min(3, { message: 'Mínimo de 3 caracteres' })
+    .regex(/^([a-z\\\\-]+)$/i, { message: 'Apenas letras e hífens' })
+    .transform((username) => username.toLowerCase()),
 })
 
 // Convertendo o schema para um tipo
 type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
 
   const handleClaimUsername = (data: ClaimUsernameFormData) => {
     console.log(data)
   }
 
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        size="sm"
-        prefix="ignite.com/"
-        placeholder="seu-usuario"
-        required
-        {...register('username')}
-      />
-      <Button size="sm" type="submit">
-        Reservar
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          required
+          {...register('username')}
+        />
+
+        <Button size="sm" type="submit">
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite um nome de usuário para continuar'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
