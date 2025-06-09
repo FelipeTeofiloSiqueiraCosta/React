c857bf23919469886137d5de46c8b40c920b2efa
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2025-06-08 22:03:20 -0300

    style: registration form

---
+++ FrameWorks/NextJs/ignite-call/src/pages/register/index.page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Container, Form, Header } from './styles'
+import {
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+  TextInputProps,
+} from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+export default function RegisterPage() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <Form as="form">
+        <label>
+          <Text>Nome de usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu-usuario" />
+        </label>
+
+        <label>
+          <Text>Nome de completo</Text>
+          <TextInput placeholder="seu nome" />
+        </label>
+
+        <Button type="submit">
+          Próximo passo <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
